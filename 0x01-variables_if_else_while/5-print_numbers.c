18133c560361fde755f7b64bd6d2631f8e47e56c
Teenn1e <christinenyakinyuak@gmail.com>
2022-09-10 03:49:07 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of function
+ * Description: print single numbers
+ * Return: 0(success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("Print single numbers:%d", i);
+	printf("\n");
+return (0);
+}
