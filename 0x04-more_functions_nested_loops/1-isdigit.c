6044c30021b9cb97ed6bb44b2e99efbcc02b49c4
Teenn1e <christinenyakinyuak@gmail.com>
2022-09-18 03:08:52 -0700

    Print through 0 to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - print through 0 to 9
+ * @c: input to use
+ * Return: 1 if digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0; c <= 9)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
